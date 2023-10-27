3300596c7a0cc7bd2078d9484c497016eae85abf
Chery <cherylene40@gmail.com>
2023-10-27 18:25:52 +0700

    removing todo in tabulator

--- resources/views/pages/riwayat/rekomendasi.blade.php
+++ resources/views/pages/riwayat/rekomendasi.blade.php
@@ -1,5 +1,38 @@
 @extends('layout.side-menu')
 
 @section('subcontent')
-    <p>Riwayat Rekomendasi</p>
+    <div class="intro-y flex flex-col sm:flex-row items-center mt-8">
+        <h2 class="text-lg font-medium mr-auto">
+            Riwayat Rekomendasi
+        </h2>
+    </div>
+
+    <div class="intro-y box p-5 mt-5">
+        <div class="overflow-x-auto scrollbar-hidden">
+            <table id="table" class="stripe hover overflow-x-auto">
+                <thead>
+                    <tr>
+                        <th class="border-b-2 whitespace-nowrap">Name</th>
+                        <th class="border-b-2 whitespace-nowrap">Category</th>
+                        <th class="border-b-2 whitespace-nowrap">Remaining Stock</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @for ($i = 1; $i < 10; $i++)
+                        <tr>
+                            <td class="border-b">Item {{ $i }}</td>
+                            <td class="border-b">Category {{ $i }}</td>
+                            <td class="border-b">{{ $i * 10 }}</td>
+                        </tr>
+                    @endfor
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
+
+@push('scripts')
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+    @vite('resources/js/pages/datatable.js')
+@endpush
