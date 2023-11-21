3f38e57ceb52713ac7c654a4ede76444bd9e0e52
Chery <cherylene40@gmail.com>
2023-11-21 21:20:59 +0700

    Refactor layout in pemupukan.blade.php and pengairan.blade.php

--- resources/views/pages/rekomendasi/pemupukan.blade.php
+++ resources/views/pages/rekomendasi/pemupukan.blade.php
@@ -7,9 +7,32 @@
     </h2>
 </div>
 
-<div class="intro-y box p-5 mt-5">
-    <div class="overflow-x-auto scrollbar-hidden">
-        abcdef
+    {{-- Wanna change layout? gambar kiri, kanan grafik + action --}}
+    {{-- <div class="flex flex-col min-h-[68vh] lg:flex-row mt-5 gap-4">
+        <div class="intro-y basis-1/2 box p-5">
+            ini gambar
+        </div>
+        <div class="basis-1/2 flex flex-col gap-3">
+            <div class="intro-y basis-1/2 box p-5">
+                ini grafik
+            </div>
+            <div class="intro-y basis-1/2 box p-5">
+                ini action
+            </div>
+        </div>
+    </div> --}}
+
+    <div class="flex flex-col min-h-[68vh] mt-5 gap-4">
+        <div class="intro-y grow basis-1/2 box p-5">
+            ini gambar
+        </div>
+        <div class="intro-y grow basis-1/2 flex flex-col lg:flex-row gap-3">
+            <div class="basis-1/2 box p-5">
+                ini grafik
+            </div>
+            <div class="intro-y basis-1/2 box p-5">
+                ini action
+            </div>
+        </div>
     </div>
-</div>
 @endsection
