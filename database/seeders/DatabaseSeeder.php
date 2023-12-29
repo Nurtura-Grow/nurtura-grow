c007847b34087ecfa45fe73c4c8f333afa27fabd
Chery <cherylene40@gmail.com>
2023-12-29 22:25:20 +0700

    Add SOPPengairan model, migration, and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             LahanSeeder::class,
             TipeInstruksiSeeder::class,
             SopPemupukanSeeder::class,
+            SopPengairanSeeder::class,
             // DataSensorSeeder::class,
             // PrediksiSensorSeeder::class,
         ]);
